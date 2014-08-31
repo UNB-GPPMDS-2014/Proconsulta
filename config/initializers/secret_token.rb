9b571f420b6fd9d3c662ec5cb4063462859cbdd5
Matheus S. Pereira <matheussilva.fga@gmail.com>
2014-08-31 18:10:49 -0300

    Iniciando o projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SISANPRO::Application.config.secret_token = 'c4ce81c3f4cf819b718dae11983d1fb548f53a633180b0464d247cb7d79942987f5b802fcdcc4d041fa1520f9389bb66b51605f283e6f198d0acb68042f55560'
