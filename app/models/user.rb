11ac1feeba11c015cf0d7df56a23e6f94a9a2c50
Matheus S. Pereira <matheussilva.fga@gmail.com>
2014-09-02 17:21:54 -0300

    Finalizando classe User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,13 @@
 class User < ActiveRecord::Base
-	attr_accessible :name_user, :adress_user, :email_user, :password_user
+
+	attr_accessible :name_user, :adress_user, :email_user, :password_user, :list_ratings, :session_user
+
+	list_ratings = Array.new
+
+	has_many :ratings
+
+	has_one :session
+
+	validates :email_user, :password_user, presence: true
+
 end
