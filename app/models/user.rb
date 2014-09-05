9f19f4b8d38ee68d51900e17dda7028bf533d036
Dylan Guedes <djmgguedes@gmail.com>
2014-09-04 21:37:57 -0300

    better validations, now working with regex for the email and some others changes. The list_rating is now commented because is confusing at this moment

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,19 @@ class User < ActiveRecord::Base
 
 	attr_accessible :name_user, :adress_user, :email_user, :password_user, :list_ratings, :session_user
 
-	list_ratings = Array.new
+#	list_ratings = Array.new
 
 	has_many :ratings
 
 	has_one :session
 
-	validates :email_user, :password_user, presence: true
+	validates :password_user, presence: true
+	validates :name_user, presence: true, length: { maximum:50 }
+	validates :adress_user, presence: true, length: { maximum:99 }
+	validates :email_user, presence: true, length: { maximum:70 }
+ 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  	validates :email_user, presence: true, format: { with: VALID_EMAIL_REGEX },
+  			   uniqueness: { case_sensitive:false }
+  	validates :password_user, length: { minimum: 6}
 
 end
