8e83c1157ceaaacf04daaea54cffb0df26dd3196
Dylan Guedes <djmgguedes@gmail.com>
2014-09-11 22:30:31 -0300

    now everything is working based on the user_name parameter instead of :id

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,21 +11,22 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)	
     	if @user.save
-      		redirect_to @user
+      		redirect_to ('/users/'+@user.name_user)
     	else
       	render 'new'
     	end
 	end
 
 	def show
-		@user = User.find(params[:id])
+		@user = User.find_by_name_user!(params[:id])
 	end
 
 	def edit
-		@user = User.find(params[:id])
+		@user = User.find_by_name_user!(params[:name_user])
 	end
 
 	def update
+		@user = User.find_by_name_user!(params[:name_user])
 	end
 
 	def destroy
