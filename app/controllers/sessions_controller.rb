285bf480742e519e16d57b74485b1307782b2169
Eduardo Moreira <moreira.edu@hotmail.com>
2014-09-20 17:03:24 -0300

    Changing view of the logging page and creating method in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,17 @@ class SessionsController < ApplicationController
 	end
 
 	def create
+		user = User.find_by_email_user(params[:session][:email_user].downcase)
+		if user && user.authenticate(params[:session][:password])
+			#Registrar usuário e redirecionar para homepage
+		else
+			flash[:error] = 'E-mail/Senha inválida!' #Arrumar depois!!
+			render 'new'
+		end
 	end
 
 	def destroy
 	end
-	
+
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
     	if @user.save
       		redirect_to @user
     	else
-      	render 'new'
+      		render 'new'
     	end
 	end
 
