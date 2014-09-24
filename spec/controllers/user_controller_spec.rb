4227b9147aad3935d69ef389f3f940691f6c7256
Dylan Guedes <djmgguedes@gmail.com>
2014-09-24 10:15:05 -0300

    new testes for the controller users, to test new methods (post, get, etc)

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -8,6 +8,17 @@ describe UsersController, :type => :controller do
 		 password: "teste1234", adress_user: "teste",
 		 password_confirmation: "teste1234")
 	end
+	let(:valid_attributes) {
+    	skip(name_user: "UserTest", email_user: "usertest@test.com", 
+    		password: "teste1234", adress_user: "teste")
+  	}
+
+  	let(:invalid_attributes) {
+    	skip(name_user: "", email_user: "emailerrado", 
+    		password: "zzzz", adress_user: "")
+  	}
+
+  	let(:valid_session) { {} }
 
 	subject { @user }
 	it { should respond_to(:name_user) }
@@ -19,23 +30,21 @@ describe UsersController, :type => :controller do
 
 	describe "GET show" do
     
-    it "should find the account by its id" do
-      get :show, :id => @user.id
-      assigns[:user].name_user.should == "UserTes"
-    end
-  end
+	    it "should find the account by its id" do
+	      get :show, :id => @user.id
+	      assigns[:user].name_user.should == "UserTes"
+	    end
 
-	describe "GET #new" do
-		it "assigns a new atendimento as @atendimento" do
-      	get :new, {}
-      expect(assigns(:user)).to be_a_new(User)
-    end
+		describe "GET #new" do
+			it "assigns a new atendimento as @atendimento" do
+	      	get :new, {}
+	      	expect(assigns(:user)).to be_a_new(User)
+	    	end
+		end
 
-	end
+		context "with invalid attributes" do
+		end
 
-	context "with invalid attributes" do
-		it "does not save the new contact in the database"
-		it "re-renders the :new template"
 	end
 
 	describe "GET edit" do
@@ -44,6 +53,39 @@ describe UsersController, :type => :controller do
 		expect(assigns(:user)).to eq(@user)
 		end
 	end
+
+	describe "GET index" do
+		it "assigns all users as @users" do
+			user = User.create! valid_attributes
+			get :index, {}, valid_session
+			expect(assigns(:users)).to eq ([user])
+		end
+	end
+
+	describe "GET show" do
+		it "assigns all users as @users" do
+			user = User.create! valid_attributes
+			get :show, {}, valid_attributes
+			expect(assigns(:users)).to eq ([user])
+		end
+	end
+
+	describe "GET new" do
+		it "assigns all users as @users" do
+			get :new, {}, valid_session
+			expect(assigns(:user)).to be_a_new(User)
+		end
+	end
+
+	describe "POST create" do
+		describe "with valid params" do
+			it "creates a new User" do
+				expect {
+          			post :create, {:user => valid_attributes}, valid_session
+        			}.to change(User, :count).by(1)
+      		end
+      	end
+    end
 
 
 end
