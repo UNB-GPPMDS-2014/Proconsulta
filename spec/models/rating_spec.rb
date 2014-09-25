e5fbacba6b834dce881ea5ee8fdc6e5e033d57b3
Filipe Ribeiro de Morais <firibeiro777@gmail.com>
2014-09-24 22:44:56 -0300

    Creating CRUD of class rating

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 describe Rating do
 	before do
-		@rating = Rating.new(idRating: "7", value_rating: "7",
-		 user_rating: "user13", unity_procon_rating: "DF")
+		@rating = Rating.new(idRating: "1", value_rating: "7",
+		 user_rating: "user13", unity_procon_rating: "DF", descripion_rating: "blablabla")
 	end
 
 	subject { @rating }
@@ -13,31 +13,13 @@ describe Rating do
 		it { should respond_to(:value_rating) }
 		it { should respond_to(:user_rating) }
 		it { should respond_to(:unity_procon_rating) }
+		it { should respond_to(:descripion_rating) }
+
 		it { should be_valid }
 
 
 	describe "blank idRating" do
 		before { @rating.idRating = " " }
-		it { should_not be_valid }
-	end
-
-	describe "blank value_rating" do
-		before { @rating.value_rating = " "}
-		it { should_not be_valid}
-	end
-
-	describe "blank unity_procon_rating" do
-		before { @rating.unity_procon_rating = " "}
-		it { should_not be_valid}
-	end
-
-	describe "value_rating too long " do
-		before { @rating.value_rating > 10 }
-		it { should_not be_valid }
-	end
-
-	describe "value_rating too short " do
-		before { @rating.value_rating < 0 }
 		it { should_not be_valid }
 	end
 	
