e1262f462735fcd39f433a16bba8777787476025
Filipe Ribeiro de Morais <firibeiro777@gmail.com>
2014-09-24 21:49:30 -0300

    Creating some tests of rating model

--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -8,11 +8,37 @@ describe Rating do
 		 user_rating: "user13", unity_procon_rating: "DF")
 	end
 
-subject { @rating }
-	it { should respond_to(:idRating) }
-	it { should respond_to(:valeu_rating) }
-	it { should respond_to(:user_rating) }
-	it { should respond_to(:unity_procon_rating) }
-	it { should be_valid }
+	subject { @rating }
+		it { should respond_to(:idRating) }
+		it { should respond_to(:valeu_rating) }
+		it { should respond_to(:user_rating) }
+		it { should respond_to(:unity_procon_rating) }
+		it { should be_valid }
+
+
+	describe "blank idRating" do
+		before { @rating.idRating = " " }
+		it { should_not be_valid }
+	end
+
+	describe "blank valeu_rating" do
+		before { @rating.valeu_rating = " "}
+		it { should_not be_valid}
+	end
+
+	describe "blank unity_procon_rating" do
+		before { @rating.unity_procon_rating = " "}
+		it { should_not be_valid}
+	end
+
+	describe "valeu_rating too long " do
+		before { @rating.valeu_rating > 10 }
+		it { should_not be_valid }
+	end
+
+	describe "valeu_rating too short " do
+		before { @rating.valeu_rating < 0 }
+		it { should_not be_valid }
+	end
 	
 end
