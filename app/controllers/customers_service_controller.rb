dedac89cf14224c9338eecf01965e9a5c04e116a
Ricardo Lupiano Andrade <ricardo_lupiano@hotmail.com>
2014-09-25 22:30:13 -0300

    Creating method show in custome_service_controller

--- app/controllers/customers_service_controller.rb
+++ app/controllers/customers_service_controller.rb
@@ -10,3 +10,12 @@ class CustomersServiceController <ApplicationController
       format.json { render json: @CostumersService }
     end
   end
+
+  def show
+    @CostumersService = Product.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @CustomersService }
+    end
+  end
