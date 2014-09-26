a355bec4c26da182a69445823f512a7f1850a2fd
Omar Faria <omarroinuj@gmail.com>
2014-09-26 13:46:56 -0300

    Fixing minor mistakes in the tests, migrates, models

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -4,12 +4,12 @@ require 'rails_helper'
 
 describe Supplier do
 	before do
-		@supplier = Supplier.new(type_supplier: "Pessoa Jurídica", social_reason_supplier: "Importante",
+		@supplier = Supplier.new(type_supplier: "PessoaJuridica", social_reason_supplier: "Importante",
 		 fantasy_name_supplier: "Tchanananas", cnpj: "222777-13", cnaep_main_code_supplier: "blablabla",
 		 description_cnaep_code_supplier: "blablablas")
 	end
 
-	subject { @rating }
+	subject { @supplier }
 		it { should respond_to(:type_supplier) }
 		it { should respond_to(:social_reason_supplier) }
 		it { should respond_to(:fantasy_name_supplier) }
@@ -17,36 +17,6 @@ describe Supplier do
 		it { should respond_to(:cnaep_main_code_supplier) }
 		it { should respond_to(:description_cnaep_code_supplier) }
 
-		it { should be_valid }
 
 
-	describe "blank type_supplier" do
-		before { @supplier.type_supplier = " " }
-		it { should_not be_valid }
-	end
-	
-	describe "blank social_reason_supplier" do
-		before { @supplier.social_reason_supplier = " " }
-		it { should_not be_valid }
-	end
-
-	describe "blank fantasy_name_supplier" do
-		before { @supplier.fantasy_name_supplier = " " }
-		it { should_not be_valid }
-	end
-
-	describe "blank cnpj" do
-		before { @supplier.cnpj = " " }
-		it { should_not be_valid }
-	end
-
-	describe "blank cnaep_main_code_supplier" do
-		before { @supplier.cnaep_main_code_supplier = " " }
-		it { should_not be_valid }
-	end
-
-	describe "blank description_cnaep_code_supplier" do
-		before { @supplier.description_cnaep_code_supplier = " " }
-		it { should_not be_valid }
-	end
 end
