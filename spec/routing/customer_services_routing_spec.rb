3210d29e5db70f400816f0ce9a091190ef4e7ac6
Allyson Reis <allysonreis1@gmail.com>
2014-09-27 20:05:50 -0300

    Adding test 'routes to #index'.

--- spec/routing/customer_services_routing_spec.rb
+++ spec/routing/customer_services_routing_spec.rb
@@ -4,5 +4,8 @@ require 'capybara/rspec'
 
 RSpec.describe CustomerServicesController, :type => :routing do
 	
-
+	it "routes to #index" do 
+		expect(:get => "/customer_services").to route_to("customer_services#index")
+	end
+	
 end
