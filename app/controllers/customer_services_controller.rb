25a2b021adb6ca8fe397a8c5c78a2813e5e71ccd
Dylan Guedes <djmgguedes@gmail.com>
2014-10-01 10:11:41 -0300

    new pagination method for the view. now almost every customer_service shows in the index

--- app/controllers/customer_services_controller.rb
+++ app/controllers/customer_services_controller.rb
@@ -1,10 +1,12 @@
 class CustomerServicesController < ApplicationController
 
-  def index
-    @customer_services = CustomerService.first(5)
-  end
+	def index
+		@customer_services = CustomerService.paginate(page: params[:page])
+	end
 
-  def show
-    @customer_service = CustomerService.find(params[:id])
-  end
+	def show
+	    @customer_service = CustomerService.find(params[:id])
+	end
+
 end
+
