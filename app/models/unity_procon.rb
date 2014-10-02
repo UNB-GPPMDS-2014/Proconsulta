2596f012f43598a65bc252a7b6c44f452e229e1f
Ricardo Lupiano Andrade <ricardo_lupiano@hotmail.com>
2014-10-02 15:31:52 -0300

    Creating model UnityProcon

---
+++ app/models/unity_procon.rb
@@ -0,0 +1,9 @@
+class UnityProcon < ActiveRecord::Base
+	#belongs_to :uf_procon
+	has_many :ratings
+
+	attr_accessible :address_unity_procon, :name_coordinator_unity_procon,
+	 :list_ratings_unity_procon, :uf_procon, :email_unity_procon,
+	 :list_phones
+
+end
