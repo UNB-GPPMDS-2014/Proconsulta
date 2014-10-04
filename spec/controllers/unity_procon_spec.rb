2dae497b04d461a1da4f77ea9dbb23a1bc3a6e80
Ricardo Lupiano Andrade <ricardo_lupiano@hotmail.com>
2014-10-04 17:04:26 -0300

    Making test for controller unity_procon (get show)

---
+++ spec/controllers/unity_procon_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+require 'spec_helper'
+require 'capybara/rspec'
+
+describe UnityProconsController, :type => :controller do 
+  	before do
+    		@unity_procon = UnityProcon.create(address_unity_procon: "SQS BRASILIA", name_coordinator_unity_procon: "Cordenador",
+    		 uf_procon: "DF", email_unity_procon: "procon@procon.com.br", list_phones: "123123123")
+  	end
+
+    describe "GET show" do
+        it "should find the account by its id" do
+          get :show, :id => @unity_procon.id
+          assigns[:unity_procon].address_unity_procon.should == "SQS BRASILIA"
+        end
+    end
+
+
+end
