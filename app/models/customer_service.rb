869ba5b508341712f61a7ca2107b80458007eb2b
-Eduardo Moreira <moreira.edu@hotmail.com>
2014-10-07 14:23:05 -0300

    Commenting models

--- app/models/customer_service.rb
+++ app/models/customer_service.rb
@@ -1,5 +1,6 @@
 class CustomerService < ActiveRecord::Base
 	belongs_to :supplier
+	# Adding atributs to customerService
 	attr_accessible :year_customer_service, :trimester_customer_service,
 	 :month_customer_service, :date_customer_service, :region_customer_service,
 	 :uf_customer_service, :description_customer_service, :code_subject_customer_service,
