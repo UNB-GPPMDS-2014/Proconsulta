3c9a9884da0cedc2abf2493a6d07fde5f8b98ee7
Ricardo Lupiano Andrade <ricardo_lupiano@hotmail.com>
2014-10-07 14:11:32 -0300

    Adding commits in controller suppliers,unity_Procons and user

--- app/controllers/unity_procons_controller.rb
+++ app/controllers/unity_procons_controller.rb
@@ -1,9 +1,9 @@
 class UnityProconsController < ApplicationController
-
+	# Show all procon units
 	def index
 		@unity_procons = UnityProcon.paginate(page: params[:page])
 	end
-
+	# Show one especific procon unity associated with one especific id.
 	def show
 	    @unity_procon = UnityProcon.find(params[:id])
 	end
