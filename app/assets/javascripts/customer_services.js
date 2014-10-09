abf12da673eee8f6c742162537ceb8eaa857a29e
Ricardo Lupiano Andrade <ricardo_lupiano@hotmail.com>
2014-10-09 15:03:18 -0300

    Adding new html code when javascript updates customer_service search.

--- app/assets/javascripts/customer_services.js
+++ app/assets/javascripts/customer_services.js
@@ -2,16 +2,23 @@
 {
 	function custom_search(search, type_search, unities)
 	{
-		$.ajax({
+		$.ajax(
+		{
 			url: "/customer_service/custom_search",
 			type: "GET",
 			data: { "search":search, "type_search":type_search, "unities":unities,"page":1 },
-			success : function(response) {
+			success : function(response) 
+			{
 				var html = "";
+				for(var i = 0; i < response.length; i++) 
+				{
+					html += build_row(response[i]);
+				}
 				$("#customer_services").html(html);
 			}
 		});
 	}
+
 	$(document).ready(function()
 	{
 		$("#custom_search_button").click(function()
