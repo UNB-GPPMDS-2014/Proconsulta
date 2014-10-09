37a46675b843285351cf2a20426ba50e7ce860b9
Filipe Ribeiro de Morais <firibeiro777@gmail.com>
2014-10-08 22:19:04 -0300

    Creating Button and method Ranking

--- app/controllers/customer_services_controller.rb
+++ app/controllers/customer_services_controller.rb
@@ -3,7 +3,9 @@ class CustomerServicesController < ApplicationController
 	def index
 		@customer_services = CustomerService.paginate(page: params[:page])
 	end
+
 	# List Customer_Service related to one especific id.
+
 	def show
 	    @customer_service = CustomerService.find(params[:id])
 	end
