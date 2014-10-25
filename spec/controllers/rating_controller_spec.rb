4b905467a10f0a9efecb0d69a517bcb7169b2b69
DylanGuedes <djmgguedes@gmail.com>
2014-10-25 12:32:12 -0200

    changing unity_procon controller spec name, doing the pattern for the sessions_controller_spec

---
+++ spec/controllers/rating_controller_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+require 'spec_helper'
+require 'capybara/rspec'
+
+describe RatingsController, :type => :controller do 
+	
+
+
+end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,6 +3,30 @@ require 'spec_helper'
 require 'capybara/rspec'
 
 describe SessionsController, :type => :controller do 
+	describe "POST destroy" do
+		it "expect destroy to delete current session" do
+		end
+
+		it "expect destroy to redirect to root_url" do
+		end
+	end
+
+	describe "POST create" do
+		it "expect user to be finded by email" do
+		end
+
+		it "expect user finded to be signed" do
+		end
+
+		it "expect signed user to be redirected to his profile" do
+		end
+
+		it "expect invalid user to return error message" do
+		end
+
+		it "expect invalid user to rerender the session view" do
+		end
+	end
 
 
 end
