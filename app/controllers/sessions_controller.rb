1a5041962e16e6282de522fc2d94cbf3ca9dbe8d
DylanGuedes <djmgguedes@gmail.com>
2014-10-29 20:29:54 -0200

    fixed how the shared_messages are displayed @ layouts/application.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,9 +8,10 @@ class SessionsController < ApplicationController
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
 			redirect_to user
-		else
-			flash.now[:error] = "Invalid email/password combination"
-      		render 'new'
+			flash[:sucess] = "Logado com exito!"
+		else	
+			flash.now[:error] = "Combinacao invalida de email e password."		
+      		render 'new'      		
 		end
 	end
 	# Sign out
