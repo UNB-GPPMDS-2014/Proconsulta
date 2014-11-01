774976f34eef5bd29923a81d44849fa63d7d57ee
=Eduardo Moreira <moreira.edu@hotmail.com>
2014-11-01 16:34:50 -0200

    Creating method custom_search in supplier's controller

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -8,5 +8,27 @@ class SuppliersController < ApplicationController
   def show
     @supplier = Supplier.find(params[:id])
   end
+  def custom_search
+		unless request.xhr? or params[:page].nil? or params[:search].nil?
+			redirect_to "/"
+			return
+		end
+
+		sql = "1=1"
+
+		if !params[:type_search].nil? !
+			sql += "AND #{params[:type_search]}_suppliers = ?"
+		end
+
+		if !params[:unities].nil? and params[:unities].length > 0
+			params[:unities].each do |u|
+
+			end
+		end
+
+		sql = sql
+		data = Supplier.where("#{params[:type_search]}_suppliers = ?", params[:search]).paginate(:page=>1)
+		render :json=>data.to_json
+	end
 
 end
