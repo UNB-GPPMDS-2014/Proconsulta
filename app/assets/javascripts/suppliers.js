605ab8bac30b5250139e655c4d8a358e4f4071d2
=Eduardo Moreira <moreira.edu@hotmail.com>
2014-11-01 16:51:40 -0200

    Creating function to search suppliers by the given parameters

--- app/assets/javascripts/suppliers.js
+++ app/assets/javascripts/suppliers.js
@@ -1,6 +1,24 @@
 (function()
 {
 
+	function custom_search(search, type_search, suppliers)
+	{
+		$.ajax(
+		{
+			url: "/supplier/custom_search",
+			type: "GET",
+			data: { "search":search, "type_search":type_search, "suppliers":suppliers,"page":1 },
+			success : function(response) 
+			{
+				var html = "";
+				for(var i = 0; i < response.length; i++) 
+				{
+					
+				}
+				$("#suppliers").html(html);
+			}
+		});
+	}
 	$(document).ready(function()
 	{
 		$("#supplier_button").click(function()
@@ -9,7 +27,7 @@
 			var type_search = $(".search_type:checked").val();
 			var suppliers = [];
 
-			
+			custom_search(search, type_search, suppliers);
 
 		});
 	});
