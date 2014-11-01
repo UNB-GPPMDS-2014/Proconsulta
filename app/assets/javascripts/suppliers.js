11354f2f3a6c7f7c3e23e22be288aceb455fdfb6
Ricardo Lupiano Andrade <ricardo_lupiano@hotmail.com>
2014-11-01 16:43:44 -0200

    Creating javascript code for supplier's search

---
+++ app/assets/javascripts/suppliers.js
@@ -0,0 +1,16 @@
+(function()
+{
+
+	$(document).ready(function()
+	{
+		$("#supplier_button").click(function()
+		{
+			var search = $("#supplier_text").val();
+			var type_search = $(".search_type:checked").val();
+			var suppliers = [];
+
+			
+
+		});
+	});
+})();
