6443ebb1d69c9e16be2830c683e5026e508e27be
DylanGuedes <djmgguedes@gmail.com>
2014-11-02 20:01:24 -0200

    refactored organization of the methods. now they are organized by their functions

--- features/step_definitions/authentication_steps.rb
+++ features/step_definitions/authentication_steps.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 
+#common methods
 Dado(/^que eu estou na homepage$/) do
   visit root_path
 end
@@ -8,32 +9,43 @@ Quando(/^eu clico no link "(.*?)"$/) do |arg1|
   click_link arg1
 end
 
-Entao(/^vejo o formulario de cadastro$/) do
-  assert page.has_text?("Nome")
-  assert page.has_text?("E-mail")
-  assert page.has_text?("Senha")
-  assert page.has_text?("Repetir")
-end
-
 Quando(/^eu preencho o campo "(.*?)" com "(.*?)"$/) do |arg1, arg2|
   pending # teste
+end
+
+Quando(/^eu clico no atendimento "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
+end
+
+Entao(/^erros aparecem$/) do
+  pending # express the regexp above with the code you wish you had
 end
 
 E(/^preencho o campo "(.*?)" com "(.*?)"$/) do |arg1, arg2|
   fill_in arg1, with: arg2
 end
 
+Quando(/^eu preencho "(.*?)" em "(.*?)"$/) do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
 E(/^clico no botão "(.*?)"$/) do |arg1|
   click_button arg1
+end
+
+Quando(/^aperto "(.*?)"$/) do |arg1|
+  pending # express the regexp above with the code you wish you had
 end
 
 Entao(/^vejo "(.*?)"$/) do |arg1|
   assert page.has_text?(arg1)
 end
 
+
 Entao(/^vejo o texto "(.*?)"$/) do |arg1|
   assert page.has_text(arg1)
 end
+
 
 Entao(/^sou redirecionado para a pagina "(.*?)"$/) do |arg1|
   pending # pendente
@@ -43,19 +55,24 @@ Entao(/^erros aparecem"$/) do
   page.should have_selector('div.alert.alert-error')
 end
 
+#'cadastrar' methods
+Entao(/^vejo o formulario de cadastro$/) do
+  assert page.has_text?("Nome")
+  assert page.has_text?("E-mail")
+  assert page.has_text?("Senha")
+  assert page.has_text?("Repetir")
+end
+
+#'listar atendimento' methods
+
 Entao(/^vejo a lista de atendimentos$/) do
-  pending # express the regexp above with the code you wish you had
+  assert page.has_text?("Ano")
+  assert page.has_text?("UF")
+  assert page.has_text?("ID")
+  assert page.has_text?("Descrição")
 end
 
 Entao(/^vejo os filtros para pesquisar atendimento$/) do
-  pending # express the regexp above with the code you wish you had
-end
-
-Quando(/^eu preencho "(.*?)" em "(.*?)"$/) do |arg1, arg2|
-  pending # express the regexp above with the code you wish you had
-end
-
-Quando(/^aperto "(.*?)"$/) do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
@@ -68,11 +85,5 @@ Entao(/^vejo a lista de atendimentos com um atendimento:$/) do |table|
   pending # express the regexp above with the code you wish you had
 end
 
-Quando(/^eu clico no atendimento "(.*?)"$/) do |arg1|
-  pending # express the regexp above with the code you wish you had
-end
 
-Entao(/^erros aparecem$/) do
-  pending # express the regexp above with the code you wish you had
-end
 
