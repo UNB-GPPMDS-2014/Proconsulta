6c3de6580afa80c900e0fe01b899862b296f7e7d
DylanGuedes <djmgguedes@gmail.com>
2014-11-05 09:04:26 -0200

    pass confirmation and invalid mail shared_messages fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ActiveRecord::Base
 	attr_accessible :email_user, :name_user, :password, :password_confirmation, :address_user
 	has_secure_password
+  after_validation { self.errors.messages.delete(:password_digest) }
   before_save { |user| user.email_user = email_user.downcase }
   before_save :create_remember_token
 #	list_ratings = Array.new
 	has_many :ratings
 
 
-	validates :password, presence: true
 	validates :name_user, presence: true, length: { maximum:50 }
 	validates :address_user, presence: true, length: { maximum:2 }
-	validates :email_user, presence: true, length: { maximum:70 }
+	validates :email_user, length: { maximum:70 }
  	valid_email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email_user, presence: true, format: { with: valid_email_regex },
+  validates :email_user, format: { with: valid_email_regex },
   			   uniqueness: { case_sensitive:false }
   validates :password, length: { minimum: 6}
 
