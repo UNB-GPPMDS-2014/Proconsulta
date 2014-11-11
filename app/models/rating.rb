611848e9ee42f58eb6ddcaf03db42b6c165c88e8
ludimila <lbelacruz@gmail.com>
2014-11-10 23:53:16 -0200

    Fixing name of atributes

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,8 +1,6 @@
 class Rating < ActiveRecord::Base
 	# Adding atributs to Ratings model
-	attr_accessible :value_rating, :unity_procon_rating, :descripion_rating
+	attr_accessible :value_rating, :unity_procon_rating, :description_rating
 
 	belongs_to :user
-	belongs_to :unity_procon
-
 end
