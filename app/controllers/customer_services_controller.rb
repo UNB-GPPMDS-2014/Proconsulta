4996f4c45ecae740831788355df676591eccb1b1
Eduardo Moreira <moreira.edu@hotmail.com>
2014-11-12 23:30:35 -0200

    Creating methods to return all regions and to filter by given type

--- app/controllers/customer_services_controller.rb
+++ app/controllers/customer_services_controller.rb
@@ -4,6 +4,7 @@ class CustomerServicesController < ApplicationController
 		@customer_services = CustomerService.paginate(page: params[:page])
 		@hash_uf = return_hash
 		@hash_uf_filter = Hash.new
+		@hash_region = return_hash_region
 	end
 
 	def list
@@ -97,6 +98,63 @@ class CustomerServicesController < ApplicationController
 
 				when QUANTITY_SIMPLE_CONSULT
 					then uf.quantity_simple_consult
+					
+			end
+		end	
+
+		render :json => hash.to_json
+	end
+
+	def return_hash_region
+		hash = Hash.new
+		all_region = Region.all
+
+		all_region.each do |region|
+			if (region.description_region == nil)
+				region.description_region = "vazio"
+			end	
+
+			hash[region.description_region] = region.quantity_region.to_i
+		end
+
+		hash
+	end
+
+	def filter_customer_service_by_type_region
+		hash = Hash.new
+		all_region = Region.all
+
+		type_service = params[:type].to_i
+
+		all_region.each do |region|
+			if (region.description_region == nil)
+				region.description_region = "vazio"
+			end
+
+			hash[region.description_region] = case type_service
+				when QUANTITY_DIRECT_COMPLAINT
+					then region.quantity_direct_complaint
+
+				when QUANTITY_PRELIMINARY_SERVICE
+					then region.quantity_preliminary_service
+
+				when QUANTITY_CALCULATION
+					then region.quantity_calculation
+
+				when QUANTITY_CIP
+					then region.quantity_direct_cip
+
+				when QUANTITY_FORWARD_SUPERVISION
+					then region.quantity_forward_supervision
+
+				when QUANTITY_INITIAL_JEC
+					then region.quantity_initial_jec
+					
+				when QUANTITY_LETTER_COMPLAINT
+					then region.quantity_letter_complaint
+
+				when QUANTITY_SIMPLE_CONSULT
+					then region.quantity_simple_consult
 					
 			end
 		end	
