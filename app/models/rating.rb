ee6a813b5e676045e3e246ca7542a063c0a7284f
Ricardo Lupiano Andrade <ricardo_lupiano@hotmail.com>
2014-11-13 15:18:26 -0200

    Deleting attribute unity_procon_rating from rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,6 @@
 class Rating < ActiveRecord::Base
 	# Adding atributs to Ratings model
-	attr_accessible :value_rating, :unity_procon_rating, :description_rating
+	attr_accessible :value_rating, :description_rating
 
 	belongs_to :user
 end
