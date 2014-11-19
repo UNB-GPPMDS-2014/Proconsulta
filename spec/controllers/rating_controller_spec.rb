0912678cfb48509bb676c302a14a4a8ab884746e
Filipe Ribeiro <firibeiro777@gmail.com>
2014-11-19 10:50:21 -0200

    new tests for ratings_controller spec

--- spec/controllers/rating_controller_spec.rb
+++ spec/controllers/rating_controller_spec.rb
@@ -1,8 +1,24 @@
 require 'rails_helper'
 require 'spec_helper'
 require 'capybara/rspec'
+include SessionsHelper
 
-describe RatingsController, :type => :controller do 
+describe RatingsController, :type => :controller do
+
+	before do
+		@user = FactoryGirl.create(:user)
+	end
+
+	describe "GET index" do
+		#controller.index
+		it "expect get to return sucess" do
+		end
+		it "expect get to redirect if user isn't signed_in" do
+			sign_in(@user)
+			get :index
+			response.should_not be_redirect
+		end
+	end 
 	
 
 
