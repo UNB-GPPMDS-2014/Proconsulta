bfe8cf4cf5ef7638c6346bd3e25cfbf9095fd210
Omar Faria <omarroinuj@gmail.com>
2014-11-28 02:00:02 -0200

    adding graph to show from supplier

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -10,6 +10,7 @@ class SuppliersController < ApplicationController
 	#Show supplier associated with one especific id.
 	def show
 		@supplier = Supplier.find(params[:id])
+        @hash = hash_format_graph
 	end
 
 	#def custom_search
@@ -30,12 +31,13 @@ class SuppliersController < ApplicationController
 
 	def hash_format_graph
 
-		@hash = @supplier.customer_services.order('count_all desc').limit(10).group(:description_problem_customer_service).count
-		array_quantity =  @hash.map { |key, value| value }
+		hash = @supplier.customer_services.order('count_all desc').limit(10).group(:description_problem_customer_service).count
+		array_quantity =  hash.map { |key, value| value }
 		quantity_10 = array_quantity.inject(:+)
 		outros = @supplier.customer_services.count - quantity_10
 		another_hash = {"outros" => outros }
-		@hash.merge(another_hash)
+		hash = hash.merge(another_hash)
+		hash
 		 
 	end
 
